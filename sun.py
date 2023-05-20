0eec72960b0be3e0dca694189a306b8dad31c361
NOLEPXXX <131864137+NolepXx@users.noreply.github.com>
2023-05-20 07:42:36 +0700

    Update sun.py

--- sun.py
+++ sun.py
@@ -410,8 +410,8 @@ for xd in range(10000):
 	j=random.randrange(4200,4900)
 	k=random.randrange(40,150)
 	l='Safari/537.36'
-	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
-	ugen.append(uaku2)
+	uaku=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+	ugen2.append(uaku)
 	
 for x in range(1000):
 	a='Mozilla/5.0 (Linux; Android 8.1.0; CPH1851) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36'
