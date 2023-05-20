f87058b4def56b5e8bb13f2611f2e2127f244968
NOLEPXXX <131864137+NolepXx@users.noreply.github.com>
2023-05-20 13:41:31 +0700

    Update sun.py

--- sun.py
+++ sun.py
@@ -641,10 +641,12 @@ for xd in range(10000) :
 	ugen2.append(uga)
 	
 def uaku():
-	try:
-	proxylist= requests.get('https://api.proxopen('.prox.txt','w').write(prox)
+#------------------[ USER-AGENT ]-------------------#
+try:
+	prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=1000&country=all&ssl=all&anonymity=all').text
+	open('.prox.txt','w').write(prox)
 except Exception as e:
-	print(' [+] Koneksi Internet Anda Tidak Ad')
+	print(' [+] Koneksi Internet Anda Tidak Terdeteksi Silahkan Cek Kuota Anda Ya Salam Dari BrayennnXD')
 prox=open('.prox.txt','r').read().splitlines()
 		#------------[ INDICATION ]---------------#
 P = '\x1b[1;97m' # PUTIH
