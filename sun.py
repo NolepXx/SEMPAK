5eddc4f6df90f6e1c772dcb6f613eeb5b6f4390c
NOLEPXXX <131864137+NolepXx@users.noreply.github.com>
2023-06-02 13:04:54 +0700

    Update sun.py

--- sun.py
+++ sun.py
@@ -1437,7 +1437,8 @@ def passwrd():
 			pwv = []
 			if len(nmf)<6:
 				if len(frs)<3:
-					pass
+
+						pass
 					else:
 						pwv.append(nmf)
 						pwv.append(frs+'1')
@@ -1455,7 +1456,7 @@ def passwrd():
 						pwv.append(frs+'09')
 						pwv.append(frs+'12')
 						pwv.append(frs+'321')
-					else:
+				else:
 					if len(frs)<3:
 						pwv.append(nmf)
 					else:
